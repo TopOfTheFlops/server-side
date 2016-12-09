9801f26684076552757d998e0b6d10c14b12a3e2
gabrielamelian <gabrielamelian@gmail.com>
2016-12-09 13:49:43 +1300

    Added passport setup file, not tested

--- auth/passportSetup.js
+++ auth/passportSetup.js
@@ -1,25 +1,61 @@
 var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
+var Strategy = require('passport-local').Strategy
+var hasher = require('./hasher')
 
-module.exports = function setup () {
-  var strategy = new LocalStrategy(function (username, password, done) {
-    var user = users.find(function (user) {
-      return user.username === username && user.password === password
+var db = require('../db/db')
+
+passport.use(new Strategy(function (username, password, done){
+  db.getUserByUsername(username)
+    .then(function(user){
+      if(user.lenght === 0){
+        done(null, false, { message: 'Incorrect username'})
+      } else {
+        hasher.checkHash(password, user[0].password, function(valid){
+          if(valid){
+            done(null, user[0])
+          } else {
+            done(null, false, { message: 'Incorrect password'})
+          }
+        })
+      }
     })
-    return done(null, user)
-  })
+    .catch(function(err){
+      console.log('Error connecting to the database', err)
+      done(err)
+    })
+}))
 
-  passport.use(strategy)
+passport.serializeUser(function(user, done){
+  done(null, user.id)
+})
 
-  // how do I go from a given user to unique piece of info I can store in the cookie.
-  passport.serializeUser(function (user, done) {
-    done(null, user.id)
-  })
+passport.deserializeUser(function (id, done){
+  db.getUserById(id)
+    .then(function(user){
+      done(null, user[0])
+    })
+    .catch(function(err){
+      console.log('Error retrieving the deserialized user from the DB')
+      done(err)
+    })
+})
 
-  // how do I find a user given the piece of information stored in the cookie
-  passport.deserializeUser(function (id, done) {
-    done(null, users.find(function (user) {
-      return user.id === id
-    }))
-  })
-}
+module.exports = passport
+
+// passport.use(new Strategy((username, password, done) => {
+//   db.getUserByUsername(username, (err, user) => {
+//     if (err) return done(err)
+//     if (!user) return done(null, false)
+//     hasher.checkHash(password, user.password, (valid) => {
+//       if (valid) return done(null, user)
+//       return done(null, false)
+//     })
+//   })
+// }))
+
+// passport.deserializeUser((id, done) => {
+//   db.getUserById(id, (err, user) => {
+//     if (err) return done(err)
+//     done(null, user)
+//   })
+// })
