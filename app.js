b9af5e62c397e27a0fac1fa1eb0f42d2afdb151c
gabrielamelian <gabrielamelian@gmail.com>
2016-12-11 12:31:18 +1300

    Changed Cors configuration to true

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 var corsOptions = {
-  origin: "*",
+  origin: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   credentials: true
